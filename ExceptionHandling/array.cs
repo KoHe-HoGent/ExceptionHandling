199bc0e49e2a2b8422184ce4266472f00e593008
KoHe-HoGent <74499802+KoHe-HoGent@users.noreply.github.com>
2020-11-19 16:45:11 +0100

    initial

---
+++ ExceptionHandling/array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class array
+        //1. Maak een eenvoudige array van 3 integers.
+            //Vraag de gebruiker welk array element getoond moet worden en toon dit element (houd er rekening mee dat de gebruiker vanaf 1 zal nummeren en niet vanaf 0). 
+            //Zorg er met try/catch voor dat ongeldige invoer correct afgehandeld wordt en de gebruiker opnieuw een element kan kiezen.
+            //Handel twee specifieke fouten af: IndexOutOfRangeException,FormatException.
+    {
+        static void Main()
+        {
+            int[] array = { 5, 10, 20 };
+            int i;
+            do {
+                Console.WriteLine("element?: ");
+                try
+                {
+                    i = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(array[i - 1]);
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Index is buiten bereik. Index moet een geheel getal zijn tussen 1 en 3");
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Index is geen integer. Index moet een geheel getal zijn tussen 1 en 3");
+                }
+            }
+            while (true);
+
+        }
+    }
+}
---
+++ ExceptionHandling/cat.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class cats
+        //2. Voorzie een Cat class met een int property: Age.
+            //Maak een eigen error type CustomCatError aan dat overerft van System.ApplicationException.
+            //Maak een lijst van Catobjecten aan, geef deze lijst door aan een method die de leeftijd van de katten controleert en CustomCatError opwerpt indien er een object is met leeftijd gelijk aan of kleiner dan 0.
+            //Vang dergelijke exceptions op.
+    {
+        public class Cat
+        {
+            public int Age { get; set; }
+        }
+
+        internal class CustomCatError : Exception
+        {
+            public CustomCatError()
+            {
+
+            }
+
+            public CustomCatError(string message) : base(message)
+            {
+
+            }
+
+            public CustomCatError(string message, Exception innerException) : base(message, innerException)
+            {
+
+            }
+        }
+
+        private static void AgeCheck(Cat cat)
+        {
+            if (cat.Age < 1) 
+            {
+                throw new CustomCatError();
+            }
+        }
+        static void MainCats()
+        {
+            Cat cat1 = new Cat { Age = -1 };
+            Cat cat2 = new Cat { Age = 1 };
+            Cat cat3 = new Cat { Age = 10 };
+            Cat cat4 = new Cat { Age = -10 };
+            Cat[] catLijst = { cat1, cat2, cat3, cat4 };
+
+            foreach (Cat cat in catLijst)
+            {
+                try
+                {
+                    AgeCheck(cat);
+                }
+                catch(CustomCatError)
+                {
+                    Console.WriteLine($"{cat}'s Age is 0 of lager: {cat.Age}");
+                }
+            }
+        }
+    }
+}
